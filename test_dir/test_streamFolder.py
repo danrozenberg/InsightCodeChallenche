9a538f18503b96d7a8c43ced1f98bbcb4394b37b
Dan Rozenberg <danrozenberg@gmail.com>
2015-03-08 11:52:44 -0700

    First test for stream folder passes ok.

---
+++ test_dir/test_streamFolder.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from src import DataManager
+
+class TestStreamFolder(TestCase):
+
+	def test_streamFolder(self):
+		manager = DataManager.DataManager()
+		wordList = []
+		for file in manager.streamFolder("./FolderToStream/"):
+			for line in file:
+				if line == "\n": continue
+				for word in str.rstrip(line).split():
+					wordList.append(word.lower())
+
+		#the answer list:
+		answerList = ["good", "morning", "sunshine.", "testing", "is", "fun!"
+			,"the", "third", "file.", "it", "is", "a", "little", "different.", "but", "it's", "still", "small."]
+
+		#the order doesn't matter for this test, so we sort
+		wordList.sort()
+		answerList.sort()
+
+		self.assertEquals(wordList, answerList, "Lists are not equal")
