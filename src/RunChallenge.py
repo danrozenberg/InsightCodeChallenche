5919a9b41a0118730425b0f31840293213dd51e4
Dan Rozenberg <danrozenberg@gmail.com>
2015-03-08 18:24:55 -0700

    Added comments. Runs challenge when called from run.sh

--- src/RunChallenge.py
+++ src/RunChallenge.py
@@ -1,8 +1,10 @@
 import WordCounter
 import MedianCounter
 
-inputFolder = "../wc_input/"
-outputFolder = "../wc_output/"
+"""This module runs the challenge, and is supposed to be run by the run.sh script at the root of the project
+TODO: make this runnable even if we call it from somewhere else...must fix relative paths."""
+inputFolder = "./wc_input/"
+outputFolder = "./wc_output/"
 
 WordCounter.proccessFolder(inputFolder, outputFolder + "wc_result.txt")
 MedianCounter.proccessFolder(inputFolder, outputFolder + "med_result.txt")
